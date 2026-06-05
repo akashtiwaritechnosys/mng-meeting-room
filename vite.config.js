07621cc3a4a6df524686e23af7002a01aa371ec0
akashtiwaritechnosys <akash.t@biztechnosys.com>
2026-06-05 08:51:31 +0530

    added the end point 2

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://91.126.107.22:40095',
+        target: 'http://91.126.107.22:40042',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
